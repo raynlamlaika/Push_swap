534170d1c07e276a18e0e25541bc88b7f4a669c8
Rayane Lamlaika <rlamlaik@e1r5p9.1337.ma>
2024-12-23 20:45:19 +0100

    wewe

--- stack_opperations.c
+++ stack_opperations.c
@@ -6,7 +6,7 @@
 /*   By: rlamlaik <rlamlaik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 16:17:11 by rlamlaik          #+#    #+#             */
-/*   Updated: 2024/12/23 16:17:52 by rlamlaik         ###   ########.fr       */
+/*   Updated: 2024/12/23 20:45:04 by rlamlaik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,20 +57,17 @@ void	*swap_ab(l_list **stack_b, l_list **stack_a)
 }
 
 //pa (push a): Take the first element at the top of b and put it at the top of a.
-void	push_a(l_list *stack_b, l_list *stack_a)
+void push_a(l_list **stack_b, l_list **stack_a)
 {
-	l_list	*take_a;
-	l_list	*take_b;
+	l_list *top_b;
 
-	if (!stack_b || ft_lstsize(stack_b) < 1 || !stack_a)
-		return (0);
-	take_a = stack_a;
-	take_b = stack_b;
-	take_a->next = take_b;
-	stack_a = stack_a->next;
-	printf("lets seee %s",stack_a->data);
-	
-	return ;
+	if (!*stack_b) 
+		return;
+
+	top_b = *stack_b;
+	*stack_b = top_b->next;
+	top_b->next = *stack_a;
+	*stack_a = top_b;
 }
 
 //pb (push b): Take the first element at the top of a and put it at the top of b.
